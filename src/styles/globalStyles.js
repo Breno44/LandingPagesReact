0b55641980755bce64189a2c4529aa10d5d9a16e
Breno <bhfs.2908@gmail.com>
2021-12-23 15:43:37 -0300

    feat: create heading

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -7,5 +7,14 @@ export const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
     }
 
-    body {}
+    html {
+    font-size: 62.5%;
+  }
+  body {
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.font.family.default};
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.font.family.secondary};
+  }
 `;
